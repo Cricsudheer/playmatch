d9aeb62eca883cb855bd0ce4c52653ff6ad3cb43
Sudheer Ranjan <admin@escricket.com>
2025-08-24 21:25:19 +0530

    Initial commit

---
+++ src/test/java/com/example/playmatch/PlaymatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaymatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
